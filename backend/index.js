54b65df4ec6b5dbb1447b97eeb1542082d34bf49
sunnylin0 <druidlin@yahoo.com.tw>
2024-10-22 01:34:26 +0800

    creat sqlite

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import sqlite3 from 'sqlite3';
+import randomstring from 'randomstring'
+import path from 'path';
 import {v4 as uuidvs} from 'uuid';
 import seedDatabase from './seedDatabase.js';
 import {
@@ -7,7 +9,10 @@ import {
 	mdb_sql1, mdb_table1
 } from './mdbtools.js';
 
+const __dirname = path.resolve();
+
 await seedDatabase();
+
 
 const app = express();
 const port = 8080;
@@ -28,33 +33,44 @@ app.get("/mdbinit", (req, res) => {
 });
 app.get("/mdb2", (req, res) => {
 	console.log("mdbinit function 2");
-	mdb2();
-	res.send("mdbinit function 2");
+	let result=mdb2();
+	res.send("mdbinit function 2\n" +
+		JSON.stringify(result).toString());
 });
 app.get("/mdb3", (req, res) => {
 	console.log("mdb table function 3");
-	mdb3();
-	res.send("mdb tabld function 3");
+	let result=mdb3();
+	res.send("mdb tabld function 3\n" + 
+		JSON.stringify(result).toString());
 });
 app.get("/quer", (req, res) => {
 	console.log("mdb table function mdb_quer");
-	mdb_quer();
-	res.send("mdb tabld function mdb_quer");
+	let result =mdb_quer();
+	res.send("mdb tabld function mdb_quer"+
+		JSON.stringify(result).toString());
 });
 app.get("/sql1", (req, res) => {
 	console.log("mdb table function sql 1");
-	mdb_sql1();
-	res.send("mdb tabld function sql 1");
+	let result = mdb_sql1();
+	console.log("result=")
+	console.log(result)
+
+	console.log("JSON.stringify(result).toString()=")
+	console.log(JSON.stringify(result).toString())
+	res.send("mdb tabld function sql 1\n" +
+		JSON.stringify(result).toString());
 });
-
-
-
 
 app.get("/tab1", (req, res) => {
 	console.log("mdb table function mdb_table1");
-	mdb_table1();
-	res.send("mdb tabld function mdb_table1");
+	let result =mdb_table1();
+	res.send("mdb tabld function mdb_table1"+
+		JSON.stringify(result));
 });
+
+
+
+
 
 
 app.get("/health", (req, res) => {
@@ -103,3 +119,61 @@ async function closeGracefully(signal) {
 }
 process.on("SIGINT", closeGracefully);
 process.on("SIGTERM", closeGracefully);
+
+
+
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+	delete req.headers['X-Frame-Options'];
+	res.sendFile(path.join(__dirname + '/index.html'))
+});
+
+
+const GENDER = ["MALE", "FEMALE"]
+let count = 0;
+
+app.get('/api/user', (req, res) => {
+	delete req.headers['X-Frame-Options'];
+	count += 1;
+	const { query: { requestId } } = req;
+	const responseJson = {
+		id: randomstring.generate(13),
+		adId: requestId,
+		salutation: GENDER[Math.round(Math.random())],
+		firstname: randomstring.generate({
+			length: 5,
+			charset: 'alphabetic'
+		}),
+		lastname: randomstring.generate({
+			length: 5,
+			charset: 'alphabetic'
+		}),
+		birthday: "1999-12-25",
+		phoneNumber: "0049" + randomstring.generate({
+			length: 9,
+			charset: 'numeric'
+		}),
+		email: randomstring.generate({
+			length: 5,
+			charset: 'alphabetic'
+		}) + randomstring.generate({
+			length: 4,
+			charset: 'numeric'
+		}) + "@ssssss.sdf",
+		schufaAgreementAccepted: (Math.round(Math.random()) > 0),
+		newsletterSubscription: (Math.round(Math.random()) > 0)
+	}
+	console.log(responseJson)
+		res.json(responseJson)
+
+
+});
+
+//app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- database/export2.ps1
+++ database/export2.ps1
@@ -3,31 +3,42 @@
 # $path = (Get-ChildItem "jsd djej sjdj ajf").value.split(" ")
 param($fname,$outdb)  # 取得外部 %1 %2
 $fname
-$outdb.length()
+$outdb
 $i=0
 $dir ='sql_' + (Get-Date -Format "MMdd_HH")
 $dir
 Remove-Item -path $dir -recurse
 Remove-Item $outdb -recurse
 mkdir $dir
-mdb-schema $fname sqlite > $dir/schema
+$scm=mdb-schema --default-values $fname mysql  #> $dir/schema
+#$scm=mdb-schema --indexes --relations --default-values --not-null $fname  sqlite
+foreach ($n in $scm){
+	#$n.gettype()
+	$one=$n.Replace("CREATE TABLE","CREATE TABLE IF NOT EXISTS ")
+	$one >> $dir/schema
+}
+ 
+
 $tables =(mdb-tables.exe $fname).Trim().split(' ')
 Write-Host "tables name"
 foreach ($n in $tables)
 {
 	Write-Host  table name --> $n  .. 
-	$outSQL = mdb-export -D "%Y-%m-%d %H:%M:%S" -H -I sqlite $fname $n 
-	$outSQL = $outSQL.replace('"',"'")
+	$outSQL = mdb-export -D "%Y-%m-%d %H:%M:%S" -H -I mysql $fname $n 
+	if ($outSQL.Length -gt 10){
+		$outSQL = $outSQL.replace('"',"'")
+	}
 	$outSQL> $dir/$n.sql
 }
 
 get-content $dir/schema | sqlite3 $outdb
 
-$folders = Get-ChildItem  $dir\  -Recurse -Filter *.sql
-foreach ($lis in $folders) {
-	$lis.FullName
-	get-content $lis.FullName | sqlite3 $outdb
-}
+#
+#$folders = Get-ChildItem  $dir\  -Recurse -Filter *.sql
+#foreach ($lis in $folders) {
+#	$lis.FullName
+#	get-content $lis.FullName | sqlite3 $outdb
+#}
 
 #foreach ($f in $file)
 #{
